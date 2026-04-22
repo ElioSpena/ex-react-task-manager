8fbdcf47261250b67a7e491ec19470c3b6615e4e
ElioSpena <elio.spena.94@gmail.com>
2026-04-22 19:21:21 +0200

    feat: creato form in addTask e validazione title

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,5 +1,65 @@
+import { useState, useRef } from "react";
+
 export default function AddTask() {
-    return(
-        <div>addtask</div>
-    )
+  const [title, setTitle] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const description = useRef(null);
+  const status = useRef(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTask = {
+      title,
+      description: description.current.value,
+      status: status.current.value,
+    };
+
+    if (!title.trim()) {
+      setErrorMessage("Campo obbligatorio!");
+      return;
+    }
+    console.log(newTask);
+
+    setTitle("");
+    description.current.value = "";
+    status.current.value = "to-do";
+  }
+
+  function handleChange(e) {
+    const value = e.target.value;
+    const symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~";
+
+    if ([...value].some((c) => symbols.includes(c))) {
+      setErrorMessage("Il titolo non può contenere caratteri speciali!");
+      return;
+    }
+    setErrorMessage("");
+    setTitle(value);
+  }
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Nome</label>
+          <input value={title} onChange={handleChange} type="text" id="title" />
+          {errorMessage && <span>{errorMessage}</span>}
+        </div>
+        <div>
+          <label htmlFor="description">Descrizione</label>
+          <textarea ref={description} id="description" />
+        </div>
+        <div>
+          <label htmlFor="status">Status</label>
+          <select ref={status} id="status" defaultValue="to-do">
+            <option value="to-do">To do</option>
+            <option value="doing">Doing</option>
+            <option value="done">Done</option>
+          </select>
+        </div>
+        <button type="submit">Invia</button>
+      </form>
+    </section>
+  );
 }
