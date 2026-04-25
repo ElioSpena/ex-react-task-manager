1dbd6ee36887db90dfaba171b5ef1113a58612e2
ElioSpena <elio.spena.94@gmail.com>
2026-04-25 18:20:49 +0200

    feat: creato componente modale  conferma eliminazione task

---
+++ src/components/Modal.jsx
@@ -0,0 +1,44 @@
+import { createPortal } from "react-dom";
+
+export default function Modal({
+  title,
+  content,
+  show,
+  onClose,
+  onConfirm,
+  confirmText,
+}) {
+  const modalRoot = document.getElementById("modal");
+  return (
+    show &&
+    createPortal(
+      <>
+        <div className="modal-backdrop fade show"></div>
+        <div className="modal d-block " tabIndex="-1">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">{title}</h5>
+              </div>
+
+              <div className="modal-body">
+                <p>{content}</p>
+              </div>
+
+              <div className="modal-footer">
+                <button className="btn btn-secondary" onClick={onClose}>
+                  Annulla
+                </button>
+
+                <button className="btn btn-danger" onClick={onConfirm}>
+                  Conferma
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>,
+      modalRoot,
+    )
+  );
+}
--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,24 +1,27 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useGlobal } from "../context/GlobalContext";
+import Modal from "../components/Modal";
+import { useState } from "react";
 
 export default function TaskDetail() {
   const { tasksData } = useGlobal();
   const { id } = useParams();
   const navigate = useNavigate();
+  const [show, setShow] = useState(false);
 
   const task = tasksData.tasks.find((t) => t.id === Number(id));
 
-  async function handleClick() {
+  async function handleDelete() {
     try {
       const resp = await tasksData.removeTask(Number(id));
-      alert("Task eliminata!");
-      navigate("/task-list");
+
+      !show && navigate("/task-list");
     } catch (err) {
       alert(err.message);
     }
   }
 
-  if (!task) return <h2>Task non trovata!</h2>;
+  if (!task) return <h2 className="text-center">Task non trovata!</h2>;
   return (
     <section className="container py-4 d-flex justify-content-center">
       <article className="card w-100" style={{ maxWidth: "600px" }}>
@@ -35,11 +38,19 @@ export default function TaskDetail() {
             {task.createdAt}
           </span>
 
-          <button onClick={handleClick} className="btn btn-danger">
+          <button onClick={() => setShow(true)} className="btn btn-danger">
             Cancella
           </button>
         </div>
       </article>
+
+      <Modal
+        show={show}
+        title={task.title}
+        content={task.description}
+        onClose={() => setShow(false)}
+        onConfirm={handleDelete}
+      />
     </section>
   );
 }
