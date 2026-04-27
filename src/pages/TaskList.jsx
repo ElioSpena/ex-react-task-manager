0f2582eb8183630c8a883ce11f990158f8897e6f
ElioSpena <elio.spena.94@gmail.com>
2026-04-27 15:57:47 +0200

    feat: aggiunto ordinamento task in TaskList e logica di ordinamento con useMemo

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,9 +1,47 @@
 import { useGlobal } from "../context/GlobalContext";
+import { useState } from "react";
 import TaskRow from "../components/TaskRow";
+import { useMemo } from "react";
 
 export default function TaskList() {
   const { tasks } = useGlobal();
+  const [sortBy, setSortBy] = useState("createdAt");
+  const [sortOrder, setSortOrder] = useState(1);
 
+  function handleClick(value) {
+    if (sortBy === value) {
+      setSortOrder((prev) => (prev === 1 ? -1 : 1));
+    } else {
+      setSortBy(value);
+      setSortOrder(1);
+    }
+  }
+
+  const sortedTasks = useMemo(() => {
+    const sorted = [...tasks];
+
+    if (sortBy === "title") {
+      sorted.sort((a, b) => a.title.localeCompare(b.title) * sortOrder);
+    }
+
+    if (sortBy === "status") {
+      const order = ["To do", "Doing", "Done"];
+      sorted.sort(
+        (a, b) =>
+          (order.indexOf(a.status) - order.indexOf(b.status)) * sortOrder,
+      );
+    }
+
+    if (sortBy === "createdAt") {
+      sorted.sort(
+        (a, b) =>
+          (new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()) *
+          sortOrder,
+      );
+    }
+
+    return sorted;
+  }, [tasks, sortBy, sortOrder]);
 
   return (
     <section className="container py-4 d-flex justify-content-center">
@@ -11,14 +49,16 @@ export default function TaskList() {
         <table className="table table-striped table-hover table-bordered align-middle">
           <thead className="table-light text-center">
             <tr>
-              <th>Nome</th>
-              <th>Stato</th>
-              <th>Data di Creazione</th>
+              <th onClick={() => handleClick("title")}>Nome</th>
+              <th onClick={() => handleClick("status")}>Stato</th>
+              <th onClick={() => handleClick("createdAt")}>
+                Data di Creazione
+              </th>
             </tr>
           </thead>
 
           <tbody>
-            {tasks.map((task, id) => (
+            {sortedTasks.map((task, id) => (
               <TaskRow key={id} task={task} />
             ))}
           </tbody>
